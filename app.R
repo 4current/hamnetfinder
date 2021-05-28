365568625d4dd893573b08d124e0ea94ab109643
Richard Morrisey <r.morrisey@computer.org>
2021-05-28 12:51:30 -0400

    cache gets, clean ui

--- app.R
+++ app.R
@@ -28,13 +28,14 @@ ui <- fluidPage(
             "zone",
             "Time Zone",
             c("Eastern","Central","Mountain","Pacific")),
-          dateInput("date", "date", value = NULL, min = NULL, max = NULL,
+          dateInput("date", "Day", value = NULL, min = NULL, max = NULL,
               format = "yyyy-mm-dd", startview = "month", weekstart = 0,
               language = "en", width = NULL)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
+          strong(h4(textOutput("dayLabel"))),
           tableOutput("listings")
         )
     )
@@ -43,22 +44,34 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   days <- c("Su","Mo","Tu","We","Th","Fr","Sa")
+  
+  get_todays_ham_nets <- function(zone) {
+    # Gets today's xlsx file by time zone if it is not already on disk
+    # Always returns the name of the data file
+    thisDay <- Sys.Date()
+    data_file <- paste0(zone,"_ham_nets_",thisDay,".Rda")
+    if ( !file.exists(data_file) ) {
+      xlsx_url <- paste0('https://www.theweatherwonder.com/Net_List_Spreadsheet_',zone,'_Time.xlsx')
+      GET(xlsx_url, write_disk(tf <- tempfile(fileext = ".xlsx")))
+      test <- read_excel(tf, sheet = 1)
+      col_type_vec <- c("text", rep("guess", times=7), "date", "skip", rep("text", times=3),"skip")
+      xlsx <- read_excel(tf, sheet=1, col_types=col_type_vec[1:ncol(test)], skip =1)
+      xlsx[,2:8] <- !is.na(xlsx[,2:8])
+      xlsx[[zone]] <- format(xlsx[[zone]], format='%-I:%M %p')
+      save(xlsx, file=data_file)
+    }
+    data_file
+  }
+  
+  output$dayLabel <- reactive({
+    format(input$date, format="%A, %B %-d, %Y")
+  })
+  
   output$listings <- renderTable({
-    zone <- input$zone
-    xlsx_url <- paste0('https://www.theweatherwonder.com/Net_List_Spreadsheet_',zone,'_Time.xlsx')
-    data_filename <- paste0(zone,"_ham_net_data.Rda")
-    GET(xlsx_url, write_disk(tf <- tempfile(fileext = ".xlsx")))
-    test <- read_excel(tf, sheet = 1)
-    col_type_vec <- c("text", rep("guess", times=7), "date", "skip", rep("text", times=3),"skip")
-    xlsx <- read_excel(tf, sheet=1, col_types=col_type_vec[1:ncol(test)], skip =1)
-    xlsx[,2:8] <- !is.na(xlsx[,2:8])
-    xlsx[[zone]] <- format(xlsx[[zone]], format='%-I:%M %p')
-    save(xlsx, file = data_filename)
-
-    load(file = data_filename)
+    load(file = get_todays_ham_nets(input$zone))
     byMode <- xlsx[xlsx["Mode"]==input$modeFilter,]
     byDayAndMode <- byMode[byMode[days[as.POSIXlt(input$date)$wday + 1]]==TRUE,]
-    byDayAndMode[,c(zone, "Net name","Mode","Node","Comment")]
+    byDayAndMode[,c(input$zone, "Net name","Mode","Node","Comment")]
   }, na="")
 }
 
